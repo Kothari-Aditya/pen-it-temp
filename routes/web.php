93770bb162a3d3466df01973dc80b7730a1294d9
Himanshu Thakkar <himanshu@studylinkclasses.com>
2025-02-21 20:46:40 +0530

    feat: - Added edit functionality - added edit() method which gives edit form UI. - added update() method which is used to update the model in database. - Learned Route Model Binding - Note: remember about @method('PUT')

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ Route::get('/', function () {
 
 Route::get('admin/categories', [\App\Http\Controllers\admin\CategoriesController::class, 'index'])->name('admin.categories.index');
 Route::get('admin/categories/create', [\App\Http\Controllers\admin\CategoriesController::class, 'create'])->name('admin.categories.create');
+Route::get('admin/categories/{category}/edit', [\App\Http\Controllers\admin\CategoriesController::class, 'edit'])->name('admin.categories.edit');
 Route::post('admin/categories', [\App\Http\Controllers\admin\CategoriesController::class, 'store'])->name('admin.categories.store');
+Route::put('admin/categories/{category}', [\App\Http\Controllers\admin\CategoriesController::class, 'update'])->name('admin.categories.update');
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
