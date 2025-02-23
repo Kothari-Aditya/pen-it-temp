4b0c0a9dd7ccbb3e55287aa9236b394197906b16
Himanshu Thakkar <himanshu@studylinkclasses.com>
2025-02-23 19:35:27 +0530

    feature: - Made blog link to have slug

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,7 +5,7 @@
         @foreach($posts as $post)
             <div class="col-md-4 col-sm-6 col-xs-12 mb50">
                 <h4 class="blog-title">
-                    <a href="{{ route('frontend.singleBlog', $post) }}">{{ $post->title }}</a>
+                    <a href="{{ route('frontend.singleBlog', $post->slug) }}">{{ $post->title }}</a>
                 </h4>
                 <div class="blog-three-attrib">
                     <span class="icon-calendar"></span>Dec 15 2019 |
